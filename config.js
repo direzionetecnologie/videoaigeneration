812285f019da120cc01a0a5dca54a7fcb9097f33
direzionetecnologie <valentinasag2@gmail.com>
2025-10-30 11:31:55 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,85 @@
+window.CONFIG = {
+  SURVEY_TITLE: 'Test di Valutazione del Doppiaggio',
+  FORM_ENDPOINT: 'https://formsubmit.co/ajax/tech.transformation.mdst@gmail.com',
+  VIDEO_BASE_URL: 'https://aiworkshopmediaset.s3.eu-west-1.amazonaws.com/video/',
+  sections: [
+    {
+      title: 'Clip estere doppiate in italiano',
+      contents: [
+        {
+          title: 'Liquid Universe',
+          description: 'Documentario',
+          videos: [
+            'IT_B2_LIQUID_UNIVERSE_Panjaya_DUB.mp4',
+            'B2_LIQUID_UNIVERSE_IT_ElevenLabs_2025-06-20.mp4',
+            'LiquidUniverse_DoppiaggioUmano.mp4',
+            'B2_Liquid_Universe_IT-IT_VOISEED.mp4',
+            'B2_LIQUID_UNIVERSE-IT_TRANSLATED.mp4'
+          ]
+        },
+        {
+          title: 'Yargi',
+          description: 'Serie TV Turca',
+          videos: [
+            'A2_Yargi_IT_ElevenLabs_2025-09-25.mp4',
+            'A2_YARGI_IT-IT_VOISEED.mp4',
+            'A2_YARGI-IT_TRANSLATED.mp4',
+            'SegretiDiFamiglia_DoppiaggioUmano.mp4',
+            'IT_A2_YARGI_Panjaya_DUB.mp4'
+          ]
+        },
+        {
+          title: 'Chicago Fire',
+          description: 'Serie TV US',
+          videos: [
+            'A3_CHICAGO FIRE (V2)_IT_ElevenLabs_2025-09-12.mp4',
+            'A3_CHICAGO_FIRE_IT-IT_VOISEED.mp4',
+            'A3_CHICAGO_FIRE-IT_TRANSLATED.mp4',
+            'ChicagoFire_DoppiaggioUmano.mp4',
+            'IT_A3_CHICAGO_FIRE_Panjaya_DUB.mp4'
+          ]
+        },
+         {
+          title: 'La Casa Fuerte',
+          description: 'TV Show',
+          videos: [
+            'C2_LA_CASA_FUERTE_IT-IT_VOISEED.mp4',
+            'C2 LA CASA FUERTE_IT_ElevenLabs_2025-09-25.mp4'
+          ]
+        },
+      ]
+    },
+    {
+      title: 'Clip italiane doppiate in inglese',
+      contents: [
+        {
+          title: 'Viaggio nella Grande Bellezza',
+          description: 'Documentario',
+          videos: [
+            'B1_VIAGGIO_NELLA_GRANDE_BELLEZZA_EN_ElevenLabs_2025-09-15.mp4',
+            'B1_VIAGGIO_NELLA_GRANDE_BELLEZZA_EN-US_VOISEED.mp4',
+            'B1_VIAGGIO_NELLA_GRANDE_BELLEZZA-EN_TRANSLATED.mp4'
+          ]
+        },
+        {
+          title: 'Squadra Antimafia',
+          description: 'Serie TV',
+          videos: [
+            'A1_SQUADRA_ANTIMAFIA_EN_ElevenLabs_2025-09-15.mp4',
+            'A1_SQUADRA_ANTIMAFIA_EN-US_VOISEED.mp4',
+            'A1_SQUADRA_ANTIMAFIA-EN_TRANSLATED.mp4',
+            'A1_SQUADRA_ANTIMAFIA-en-US_DEEPDUB.mp4'
+          ]
+        },
+        {
+          title: 'La Casa Fuerte',
+          description: 'TV Show',
+          videos: [
+            'C2_LA_CASA_FUERTE_EN-US_VOISEED.mp4',
+            'C2 LA CASA FUERTE_EN_ElevenLabs_2025-09-25.mp4',
+          ]
+        }
+      ]
+    }
+  ]
+};
